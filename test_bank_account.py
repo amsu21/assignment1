0587447c084ec4d19fb1eb257e985452d3cdc80d
amsu21 <aswb4@hotmail.com>
2024-09-12 18:24:38 -0400

    Test file. Added test for get account number

---
+++ test_bank_account.py
@@ -0,0 +1,11 @@
+import unittest
+from bank_account import BankAccount
+
+# INIT TEST CLASS
+class TestBankAccount(unittest.TestCase):
+    
+    def setUp(self) -> None:
+        self.bank_account = BankAccount(4843985, 500.00)
+    
+    def test_get_account_number(self):
+        self.assertEqual(self.bank_account.get_account_number(), 4843985)
