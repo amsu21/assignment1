3c8fcb23c76c040e2f40aa47fd903c07be0ba6bb
amsu21 <aswb4@hotmail.com>
2024-09-12 18:52:31 -0400

    Added test functions

--- test_bank_account.py
+++ test_bank_account.py
@@ -4,8 +4,16 @@ from bank_account import BankAccount
 # INIT TEST CLASS
 class TestBankAccount(unittest.TestCase):
     
-    def setUp(self) -> None:
-        self.bank_account = BankAccount(4843985, 500.00)
+    def setUp(self):
+        self.bank_account = BankAccount("4843985", 500.00)
     
     def test_get_account_number(self):
-        self.assertEqual(self.bank_account.get_account_number(), 4843985)
+        self.assertEqual(self.bank_account.get_account_number(), "4843985")
+
+    def test_get_balance(self):
+        self.assertEqual(self.bank_account.get_balance(), 500.00)
+
+    def test_deposit(self):
+        account = BankAccount("4843985", 500.00)
+        account.deposit(25.00)
+        self.assertEqual(account.get_balance(), 525.00)
