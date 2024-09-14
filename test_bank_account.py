9ebc05c04ddd04ce3111b01504fd4f193ee0a864
amsu21 <aswb4@hotmail.com>
2024-09-13 22:45:39 -0400

    Added withdraw and deposit test functions

--- test_bank_account.py
+++ test_bank_account.py
@@ -17,3 +17,32 @@ class TestBankAccount(unittest.TestCase):
         account = BankAccount("4843985", 500.00)
         account.deposit(500.00)
         self.assertEqual(account.get_balance(), 500.00)
+    
+    def test_deposit_negative_amount(self):
+        account = BankAccount("4843985", 500.00)
+        account.deposit(-50.0)
+        self.assertEqual(account.get_balance(), 500.00)
+    
+    def test_deposit_zero_amount(self):
+        account = BankAccount("4843985", 500.00)
+        account.deposit(0.0)
+        self.assertEqual(account.get_balance(), 500.00)
+    
+    def test_withdraw(self):
+        account = BankAccount("4843985", 500.00)
+        account.withdraw(50.0)
+        self.assertEqual(account.get_balance(), 450.0)
+    
+    def test_withdraw_negative_amount(self):
+        account = BankAccount("4843985", 500.00)
+        account.withdraw(550.00)
+        self.assertEqual(account.get_balance(), 500.00)
+    
+    def test_withdraw_zero_amount(self):
+        account = BankAccount("4843985", 500.00)
+        account.withdraw(0.0)
+        self.assertEqual(account.get_balance(), 500.00)
+
+
+    if __name__ == '__main__':
+        unittest.main()
